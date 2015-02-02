94dbc243a2de77668eed5953b706a3bc8263ecfd
pance <pbarnett888@gmail.com>
2015-02-02 16:51:08 -0600

    Add mtg card master namespace

---
+++ src/clj/decktouch/mtg_card_master.clj
@@ -0,0 +1,19 @@
+(ns decktouch.mtg-card-master
+  (:require [clojure.data.json :as json]))
+
+(def mtg-cards (json/read-str (slurp "resources/AllCards.json")))
+
+(def card-names (keys mtg-cards))
+
+(defn match
+  "Returns true if query is a substring in string"
+  [query string]
+  (some? (re-matches (re-pattern (str "(?i)" query ".*"))
+                    string)))
+
+(defn get-cards-matching-query
+  "Return a list of maps of strings of card names that match the query for the card input autocomplete"
+  [query]
+  (let [match-query (partial match query)]
+      (map (fn [thing] {:cardName thing})
+        (filter match-query card-names))))
