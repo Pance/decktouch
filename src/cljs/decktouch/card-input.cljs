1d2edd11c0550a323ee14073b364ec4bc14d3f34
pance <pbarnett888@gmail.com>
2015-02-02 18:16:47 -0600

    Add cljs-ajax ns

--- src/cljs/decktouch/card-input.cljs
+++ src/cljs/decktouch/card-input.cljs
@@ -1,5 +1,6 @@
 (ns decktouch.card-input
-  (:require [reagent.core :as reagent :refer [atom]]))
+  (:require [reagent.core :as reagent :refer [atom]]
+            [ajax.core :refer [GET]]))
 
 (def llist
      [{:name "foo"}
