5aa6f1302381020d5c3f2dffd9fc837ba0554d8f
pance <pbarnett888@gmail.com>
2015-02-04 20:47:04 -0600

    Fix fn that loads cards from local storage

--- src/cljs/decktouch/card-data.cljs
+++ src/cljs/decktouch/card-data.cljs
@@ -4,10 +4,8 @@
   "Get the list of cards from localStorage, or [] if it is empty"
   (let [key "card-list"
         list (.getItem js/localStorage key)]
-    (do
-      (.log js/console key list)
-      (if (undefined? list)
-        []
-        (.parse (js/JSON))))))
+    (if (undefined? list)
+      []
+      (map (fn [n] {:name n}) (js->clj (.parse js/JSON list))))))
 
-(def cards [])
+(def cards (get-card-list-from-storage))
