9117bd4377045c483591c4e931669a94737cb784
pance <pbarnett888@gmail.com>
2015-02-08 21:36:04 -0600

    Add var for running server from a repl

--- src/clj/decktouch/handler.clj
+++ src/clj/decktouch/handler.clj
@@ -26,3 +26,6 @@
 (def app
   (let [handler (wrap-reload (wrap-params routes site-defaults))]
     (if (env :dev?) (wrap-reload (wrap-exceptions handler)) handler)))
+
+(defonce server
+  (jetty/run-jetty app {:port 8080}))
