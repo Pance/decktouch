2dfa13fe022b195992e07cdbe308e0ed721c7337
pance <pbarnett888@gmail.com>
2015-02-08 21:35:19 -0600

    Add route for getting card info

--- src/clj/decktouch/handler.clj
+++ src/clj/decktouch/handler.clj
@@ -1,8 +1,11 @@
 (ns decktouch.handler
   (:require [decktouch.dev :refer [browser-repl start-figwheel]]
-            [compojure.core :refer [GET defroutes]]
+            [compojure.core :refer [GET POST defroutes]]
             [compojure.route :refer [not-found resources]]
             [ring.middleware.defaults :refer [site-defaults wrap-defaults]]
+            [ring.middleware.params :refer [wrap-params]]
+            [ring.middleware.reload :refer [wrap-reload]]
+            [ring.adapter.jetty :as jetty]
             [selmer.parser :refer [render-file]]
             [environ.core :refer [env]]
             [prone.middleware :refer [wrap-exceptions]]
@@ -14,9 +17,12 @@
   (GET "/data/input/:query" [query]
        (json/write-str
          (mtg-card-master/get-cards-matching-query query)))
+  (POST "/data/card" request
+        (let [card-name ((request :params) "card-name")]
+          (str (mtg-card-master/get-card-info card-name))))
   (resources "/")
   (not-found "Not Found"))
 
 (def app
-  (let [handler (wrap-defaults routes site-defaults)]
-    (if (env :dev?) (wrap-exceptions handler) handler)))
+  (let [handler (wrap-reload (wrap-params routes site-defaults))]
+    (if (env :dev?) (wrap-reload (wrap-exceptions handler)) handler)))
