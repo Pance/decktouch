a92be77283e01f0750111752845abc2cd1d88b8b
pance <pbarnett888@gmail.com>
2015-02-19 18:18:40 -0600

    Reformatting

--- project.clj
+++ project.clj
@@ -41,50 +41,65 @@
 
   :minify-assets
   {:assets
-    {"resources/public/css/site.min.css" "resources/public/css/site.css"}}
+    {"resources/public/css/site.min.css"
+     "resources/public/css/site.css"}}
 
-  :cljsbuild {:builds {:app {:source-paths ["src/cljs"]
-                             :compiler {:output-to     "resources/public/js/app.js"
-                                        :output-dir    "resources/public/js/out"
-                                        :externs       ["react/externs/react.js"
-                                                        "resources/public/js/jquery-ui.js"
-                                                        "resources/public/js/jquery-2.1.3.min.js"]
-                                        :optimizations :none
-                                        :pretty-print  true}}}}
+  :cljsbuild {
+    :builds {
+      :app {
+        :source-paths ["src/cljs"]
+        :compiler {
+          :output-to  "resources/public/js/app.js"
+          :output-dir "resources/public/js/out"
+          :externs    ["react/externs/react.js"
+                       "resources/public/js/jquery-ui.js"
+                       "resources/public/js/jquery-2.1.3.min.js"]
+          :optimizations :none
+          :pretty-print  true}}}}
 
-  :profiles {:dev {:repl-options {:init-ns decktouch.handler
-                                  :nrepl-middleware [cemerick.piggieback/wrap-cljs-repl]}
+  :profiles {
+    :dev {
+      :repl-options {
+        :init-ns decktouch.handler
+        :nrepl-middleware [cemerick.piggieback/wrap-cljs-repl]}
 
-                   :dependencies [[ring-mock "0.1.5"]
-                                  [ring/ring-devel "1.3.2"]
-                                  [pjstadig/humane-test-output "0.6.0"]]
+        :dependencies [[ring-mock "0.1.5"]
+                       [ring/ring-devel "1.3.2"]
+                       [pjstadig/humane-test-output "0.6.0"]]
 
-                   :plugins [[lein-figwheel "0.2.0-SNAPSHOT"]]
+        :plugins [[lein-figwheel "0.2.0-SNAPSHOT"]]
 
-                   :injections [(require 'pjstadig.humane-test-output)
-                                (pjstadig.humane-test-output/activate!)]
+        :injections [(require 'pjstadig.humane-test-output)
+                     (pjstadig.humane-test-output/activate!)]
 
-                   :figwheel {:http-server-root "public"
-                              :server-port 3449
-                              :css-dirs ["resources/public/css"]
-                              :ring-handler decktouch.handler/app}
+        :figwheel {:http-server-root "public"
+                   :server-port 3449
+                   :css-dirs ["resources/public/css"]
+                   :ring-handler decktouch.handler/app}
 
-                   :env {:dev? true}
+        :env {:dev? true}
 
-                   :cljsbuild {:builds {:app {:source-paths ["env/dev/cljs"]
-                                              :compiler {:source-map true}}}}}
+        :cljsbuild {
+          :builds {
+            :app {
+              :source-paths ["env/dev/cljs"]
+              :compiler {:output-to "resources/public/js/app.js"
+              :source-map true}}}}}
 
-             :uberjar {:hooks [leiningen.cljsbuild minify-assets.plugin/hooks]
-                       :env {:production true}
-                       :aot :all
-                       :omit-source true
-                       :cljsbuild {:jar true
-                                   :builds {:app
-                                             {:source-paths ["env/prod/cljs"]
-                                              :compiler
-                                              {:optimizations :advanced
-                                               :pretty-print false}}}}}
+    :uberjar {
+      :hooks [leiningen.cljsbuild minify-assets.plugin/hooks]
+      :env {:production true}
+      :aot :all
+      :omit-source true
+      :cljsbuild {:jar true
+                  :builds {
+                    :app {:source-paths ["env/prod/cljs"]
+                          :compiler {
+                            :optimizations :advanced
+                            :pretty-print false}}}}}
 
-             :production {:ring {:open-browser? false
-                                 :stacktraces?  false
-                                 :auto-reload?  false}}})
+    :production {
+      :ring {
+        :open-browser? false
+        :stacktraces?  false
+        :auto-reload?  false}}})
