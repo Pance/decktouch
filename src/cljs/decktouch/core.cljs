17656bfe4e28af1d3557f272a33532bd10ac1821
pance <pbarnett888@gmail.com>
2015-03-03 21:58:40 -0600

    Implement mana color symbol counter display

--- src/cljs/decktouch/core.cljs
+++ src/cljs/decktouch/core.cljs
@@ -60,6 +60,15 @@
   (let [mana-costs (reduce str (map #(get % "manaCost") cards))]
     (reduce record-mana-freq {"R" 0 "G" 0 "U" 0 "W" 0 "B" 0} mana-costs)))
 
+(defn mana-symbol [color number]
+  (let [mana-labels {"B" (fn [n] [:span.label.label-default (str n " " color)])
+                     "U" (fn [n] [:span.label.label-primary (str n " " color)])
+                     "G" (fn [n] [:span.label.label-success (str n " " color)])
+                     "W" (fn [n] [:span.label.label-warning (str n " " color)])
+                     "R" (fn [n] [:span.label.label-danger (str n " " color)])}]
+    ((mana-labels color) number)))
+
+
 (defn mana-cost-widget [cards]
   (let [mana-symbol-counts (get-mana-symbol-counts cards)
         sorted-mana-symbol-counts
@@ -67,7 +76,11 @@
                                    (compare [(get mana-symbol-counts key2) key2]
                                             [(get mana-symbol-counts key1) key1])))
                   mana-symbol-counts)]
-    [:div (str sorted-mana-symbol-counts)]))
+    [:div [:h3.text-right
+            (for [m (keys sorted-mana-symbol-counts)]
+              (let [n (sorted-mana-symbol-counts m)]
+                (if (> n 0)
+                  [mana-symbol m n])))]]))
 
 (defn card-counter [cards]
   [:h3 [:p.text-right (count cards) " cards"]])
