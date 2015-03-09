5ca4d6023b6605203280e672c37485e4821bc6f8
pance <pbarnett888@gmail.com>
2015-03-09 18:53:34 -0500

    Improve card image display in tooltip

--- src/cljs/decktouch/core.cljs
+++ src/cljs/decktouch/core.cljs
@@ -13,7 +13,8 @@
 ;; Views
 
 (defn img [url]
-  (str "<img src='" url "'>"))
+  [:img {:src url
+         :style (js-obj "width" "240" "height" "340")}])
 
 (defn card-in-list [card card-id]
   (let [image-name (clojure.string/replace (get card "imageName") #" " "_")
@@ -24,7 +25,7 @@
          :data-placement "bottom"
          :data-html true
          :data-trigger "hover"
-         :title (img image-link)}
+         :title (reagent/render-component-to-string [img image-link])}
          (str (get card "name"))]]))
 
 (defn card-in-list-did-mount [card-id]
