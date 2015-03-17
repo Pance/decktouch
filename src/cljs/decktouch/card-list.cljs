14681f4213db28698028013845b87d7a37caf5b9
pance <pbarnett888@gmail.com>
2015-03-17 18:40:27 -0500

    Refactor card-list component into it's own ns

---
+++ src/cljs/decktouch/card-list.cljs
@@ -0,0 +1,64 @@
+(ns decktouch.card-list
+  (:require [reagent.core :as reagent]
+            [decktouch.card-display :as card-display]))
+
+(defn card-img [url]
+  [:img {:src url
+         :style (js-obj "width" "240" "height" "340")}])
+
+(defn card-in-list [card card-id]
+  (let [multiverseId (get card "multiverseId")
+        image-link (str "https://api.mtgdb.info/content/hi_res_card_images/" multiverseId ".jpg")]
+  [:li
+    [:a {:id card-id
+         :data-toggle "tooltip"
+         :data-placement "bottom"
+         :data-html true
+         :data-trigger "hover"
+         :title (reagent/render-component-to-string [card-img image-link])
+         :onClick (fn [] (reset! card-display/url image-link))}
+         (str (get card "name"))]]))
+
+(defn card-in-list-did-mount [card-id]
+    (.popover (js/$ (str "#" card-id))))
+
+(defn card-in-list-component [card]
+  (let [card-id (str (clojure.string/replace (get card "imageName") #"\W" "_"))]
+    (reagent/create-class {:render #(card-in-list card card-id)
+                           :component-did-mount #(card-in-list-did-mount card-id)
+                           })))
+
+(defn is-card-of-type? [card type]
+  (contains? (set (get card "types")) type))
+
+(defn filter-cards-by-type [cards type]
+  (filter (fn [card] (let [c (js->clj card)]
+                       (is-card-of-type? c type))) cards))
+
+(defn remove-cards-by-type [cards type]
+  (filter #(not (is-card-of-type? % type)) cards))
+
+(defn card-type-list [cards type]
+  (let [filtered-cards-by-type (filter-cards-by-type cards type)]
+    (if (not-empty filtered-cards-by-type)
+      [:div
+        [:p type]
+        [:ul
+          (for [card (filter-cards-by-type cards type)]
+               ^{:key (get card "imageName")} [card-in-list-component card])]])))
+
+(defn component [cards]
+  (if (empty? cards)
+    [:strong "Add some cards!"]
+    ;; else
+    [:div.row
+      [:div.col-md-6
+        [card-type-list cards "Creature"]
+        [card-type-list cards "Land"]]
+      (let [noncreature-cards (remove-cards-by-type cards "Creature")]
+        [:div.col-md-6
+          [card-type-list cards "Planeswalker"]
+          [card-type-list cards "Instant"]
+          [card-type-list cards "Sorcery"]
+          [card-type-list noncreature-cards "Enchantment"]
+          [card-type-list noncreature-cards "Artifact"]])]))
--- src/cljs/decktouch/core.cljs
+++ src/cljs/decktouch/core.cljs
@@ -7,74 +7,12 @@
             [decktouch.card-data :as card-data]
             [decktouch.card-input :as card-input]
             [decktouch.widgets :as widgets]
-            [decktouch.card-display :as card-display])
+            [decktouch.card-display :as card-display]
+            [decktouch.card-list :as card-list])
   (:import goog.History))
-
 
 ;; -------------------------
 ;; Views
-
-(defn card-img [url]
-  [:img {:src url
-         :style (js-obj "width" "240" "height" "340")}])
-
-(defn card-in-list [card card-id]
-  (let [multiverseId (get card "multiverseId")
-        image-link (str "https://api.mtgdb.info/content/hi_res_card_images/" multiverseId ".jpg")]
-  [:li
-    [:a {:id card-id
-         :data-toggle "tooltip"
-         :data-placement "bottom"
-         :data-html true
-         :data-trigger "hover"
-         :title (reagent/render-component-to-string [card-img image-link])
-         :onClick (fn [] (reset! card-display/url image-link))}
-         (str (get card "name"))]]))
-
-(defn card-in-list-did-mount [card-id]
-    (.popover (js/$ (str "#" card-id))))
-
-(defn card-in-list-component [card]
-  (let [card-id (str (clojure.string/replace (get card "imageName") #"\W" "_"))]
-    (reagent/create-class {:render #(card-in-list card card-id)
-                           :component-did-mount #(card-in-list-did-mount card-id)
-                           })))
-
-(defn is-card-of-type? [card type]
-  (contains? (set (get card "types")) type))
-
-(defn filter-cards-by-type [cards type]
-  (filter (fn [card] (let [c (js->clj card)]
-                       (is-card-of-type? c type))) cards))
-
-(defn remove-cards-by-type [cards type]
-  (filter #(not (is-card-of-type? % type)) cards))
-
-(defn card-type-list [cards type]
-  (let [filtered-cards-by-type (filter-cards-by-type cards type)]
-    (if (not-empty filtered-cards-by-type)
-      [:div
-        [:p type]
-        [:ul
-          (for [card (filter-cards-by-type cards type)]
-               ^{:key (get card "imageName")} [card-in-list-component card])]])))
-
-(defn card-list [cards]
-  (if (empty? cards)
-    [:strong "Add some cards!"]
-    ;; else
-    [:div.row
-      [:div.col-md-6
-        [card-type-list cards "Creature"]
-        [card-type-list cards "Land"]]
-      (let [noncreature-cards (remove-cards-by-type cards "Creature")]
-        [:div.col-md-6
-          [card-type-list cards "Planeswalker"]
-          [card-type-list cards "Instant"]
-          [card-type-list cards "Sorcery"]
-          [card-type-list noncreature-cards "Enchantment"]
-          [card-type-list noncreature-cards "Artifact"]])]))
-
 
 (defn home-page []
   [:div.container-fluid
@@ -90,10 +28,10 @@
                     [widgets/mana-color-composition @card-data/card-list]
                     [:br]]
           [:div.row
-                    [:p.text-right (str (count (remove-cards-by-type @card-data/card-list "Creature"))) " Non-creature Spells"]
-                    [:p.text-right (str (count (filter-cards-by-type @card-data/card-list "Creature"))) " Creatures"]
-                    [:p.text-right (str (count (filter-cards-by-type @card-data/card-list "Land"))) " Lands"]]]
-        [:div.col-md-4 [card-list @card-data/card-list]]
+                    [:p.text-right (str (count (card-list/remove-cards-by-type @card-data/card-list "Creature"))) " Non-creature Spells"]
+                    [:p.text-right (str (count (card-list/filter-cards-by-type @card-data/card-list "Creature"))) " Creatures"]
+                    [:p.text-right (str (count (card-list/filter-cards-by-type @card-data/card-list "Land"))) " Lands"]]]
+        [:div.col-md-4 [card-list/component @card-data/card-list]]
         [:div.col-md-4 [card-display/component]]]])
 
 (defn about-page []
